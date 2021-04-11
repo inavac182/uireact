ca348399940c9995e1b43976f6077fc26155901f
Felipe Nava <felipe.nava.co@gmail.com>
2021-04-10 23:14:56 -0700

    test(config): make unit test to fail when a console error happens

--- jest-startup.js
+++ jest-startup.js
@@ -1,6 +1,17 @@
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { format } from 'util';
 
 import '@testing-library/jest-dom';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// eslint-disable-next-line no-undef
+console.error = function (...args) {
+  throw new Error(format(...args));
+};
+
+// eslint-disable-next-line no-undef
+console.warn = function (...args) {
+  throw new Error(format(...args));
+};
