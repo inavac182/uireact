40634ab3dfa480c74e9ab2715cf61c63e0792ef8
Felipe Nava <felipe.nava.co@gmail.com>
2021-04-20 20:02:04 -0700

    chore(navbar): adding navbar component

---
+++ packages/navbar/__tests__/navbar.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { UiNavbar } from '../src';
+import { render, screen } from '@testing-library/react';
+
+test('Should render navbar', () => {
+  render(<UiNavbar><p>TEST</p></UiNavbar>);
+
+  expect(screen.getByText('TEST')).toBeVisible();
+});
