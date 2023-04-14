b4ccb1fb0367ac98d2cc5304451fb2c4d1f6eb12
Felipe Nava <felipe.nava.co@gmail.com>
2023-04-13 23:52:55 -0700

    feat(button): adding full width prop

--- packages/button/__tests__/button.spec.tsx
+++ packages/button/__tests__/button.spec.tsx
@@ -5,73 +5,81 @@ import { UiButton } from '../src';
 
 import { uiRender } from '../../../__tests__/utils';
 
-test('executes onClick CB', () => {
-  const mock = jest.fn();
-  uiRender(<UiButton onClick={mock}>MyButton</UiButton>);
+describe('<UiButton />', () => {
+  test('executes onClick CB', () => {
+    const mock = jest.fn();
+    uiRender(<UiButton onClick={mock}>MyButton</UiButton>);
 
-  fireEvent.click(screen.getByRole('button'));
+    fireEvent.click(screen.getByRole('button'));
 
-  expect(mock).toHaveBeenCalledTimes(1);
-});
+    expect(mock).toHaveBeenCalledTimes(1);
+  });
 
-test('renders primary button', () => {
-  uiRender(<UiButton>MyButton</UiButton>);
+  test('renders primary button', () => {
+    uiRender(<UiButton>MyButton</UiButton>);
 
-  expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
-});
+    expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
+  });
 
-test('renders data test id', () => {
-  uiRender(<UiButton testId="my-button">MyButton</UiButton>);
+  test('renders data test id', () => {
+    uiRender(<UiButton testId="my-button">MyButton</UiButton>);
 
-  expect(screen.getByTestId('my-button')).toBeVisible();
-});
+    expect(screen.getByTestId('my-button')).toBeVisible();
+  });
 
-test('renders disabled', () => {
-  uiRender(
-    <UiButton testId="my-button" disabled>
-      MyButton
-    </UiButton>
-  );
+  test('renders disabled', () => {
+    uiRender(
+      <UiButton testId="my-button" disabled>
+        MyButton
+      </UiButton>
+    );
 
-  expect(screen.getByTestId('my-button')).toBeDisabled();
-});
+    expect(screen.getByTestId('my-button')).toBeDisabled();
+  });
 
-test('renders class name', () => {
-  uiRender(
-    <UiButton testId="my-button" className="something">
-      MyButton
-    </UiButton>
-  );
+  test('renders full width', () => {
+    uiRender(<UiButton fullWidth>MyButton</UiButton>);
 
-  expect(screen.getByTestId('my-button')).toHaveClass('something');
-});
+    expect(screen.getByRole('button')).toBeVisible();
+  });
 
-test('renders secondary button', () => {
-  uiRender(<UiButton buttonType="secondary">MyButton</UiButton>);
+  test('renders class name', () => {
+    uiRender(
+      <UiButton testId="my-button" className="something">
+        MyButton
+      </UiButton>
+    );
 
-  expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
-});
+    expect(screen.getByTestId('my-button')).toHaveClass('something');
+  });
 
-test('renders tertiary button', () => {
-  uiRender(<UiButton buttonType="tertiary">MyButton</UiButton>);
+  test('renders secondary button', () => {
+    uiRender(<UiButton buttonType="secondary">MyButton</UiButton>);
 
-  expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
-});
+    expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
+  });
 
-test('renders positive button', () => {
-  uiRender(<UiButton buttonType="positive">MyButton</UiButton>);
+  test('renders tertiary button', () => {
+    uiRender(<UiButton buttonType="tertiary">MyButton</UiButton>);
 
-  expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
-});
+    expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
+  });
 
-test('renders negative button', () => {
-  uiRender(<UiButton buttonType="negative">MyButton</UiButton>);
+  test('renders positive button', () => {
+    uiRender(<UiButton buttonType="positive">MyButton</UiButton>);
 
-  expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
-});
+    expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
+  });
 
-test('renders error button', () => {
-  uiRender(<UiButton buttonType="error">MyButton</UiButton>);
+  test('renders negative button', () => {
+    uiRender(<UiButton buttonType="negative">MyButton</UiButton>);
 
-  expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
+    expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
+  });
+
+  test('renders error button', () => {
+    uiRender(<UiButton buttonType="error">MyButton</UiButton>);
+
+    expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
+  });
 });
