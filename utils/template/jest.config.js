3d20d43994efce9f05c64a334cabc6498e2d4931
Felipe Nava <felipe.nava.co@gmail.com>
2023-04-14 23:58:58 -0700

    refactor(chore): update template

--- utils/template/jest.config.js
+++ utils/template/jest.config.js
@@ -1,44 +1,14 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const jestConfig = require('../../jest.config');
+
 module.exports = {
-  verbose: true,
-  modulePaths: ["node_modules"],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  modulePathIgnorePatterns: ['node_modules'],
-  moduleDirectories: ["node_modules", "src"],
-  collectCoverage: true,
-  collectCoverageFrom: [
-      "./src/**/*.{ts,tsx}",
-      "!src/**/index.ts"
-  ],
-  coverageThreshold: {
-      global: {
-          branches: 100,
-          functions: 100,
-          lines: 100,
-          statements: 100
-      }
-  },
-  coverageReporters: ['cobertura', 'html', 'text', 'text-summary'],
-  coveragePathIgnorePatterns: [
-      "<rootDir>/node_modules/",
-      "<rootDir>/__tests__/",
-      "<rootDir>/src/styles/"
-  ],
-  moduleNameMapper: {
-      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-      "\\.(css|less)$": "<rootDir>__mocks__/styleMock.js"
-  },
-  transform: {
-      '^.+\\.(j|t)sx?$': 'ts-jest'
-  },
-  globals: {
-      'ts-jest': {
-          diagnostics: true,
-          tsconfig: '../../jest.tsconfig.json'
-      }
-  },
-  setupFilesAfterEnv: ['../../jest-startup.js'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  testURL: 'http://localhost',
-  testMatch: ['<rootDir>/__tests__/**/*.spec.ts?(x)']
-}
+    ...jestConfig,
+    setupFilesAfterEnv: ['../../jest-startup.js'],
+    globals: {
+        'ts-jest': {
+            diagnostics: true,
+            tsconfig: '../../jest.tsconfig.json'
+        }
+    },
+};
