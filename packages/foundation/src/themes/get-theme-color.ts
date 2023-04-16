eb0a2c120b5c22da398b44596be1bdd1e714cb05
Felipe Nava <felipe.nava.co@gmail.com>
2023-04-16 11:44:22 -0700

    chore(config): ts config clean up (#65)

--- packages/foundation/src/themes/get-theme-color.ts
+++
@@ -1,5 +0,0 @@
-import { ColorCategories, ThemeColor, ColorTokens } from '../types/enums';
-import { Theme } from '../types';
-
-export const getThemeColor = (theme: Theme, color: ThemeColor, category: ColorCategories, token: ColorTokens): string =>
-  theme[color]?.[category]?.[token] || '';
