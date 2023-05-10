a61225027ad5f2ea5f874c51152313b4173270e7
Felipe Nava <felipe.nava.co@gmail.com>
2023-05-10 16:55:10 -0700

    feat(select): adding select input

--- packages/form/src/index.ts
+++ packages/form/src/index.ts
@@ -2,4 +2,5 @@ export type { UiCheckboxProps, UiTextAreaProps, UiInputProps } from './types';
 
 export * from './ui-input';
 export * from './ui-checkbox';
+export * from './ui-select';
 export * from './ui-textarea';
--- packages/form/src/theme/index.ts
+++ packages/form/src/theme/index.ts
@@ -1,4 +1,5 @@
 export * from './category-helper';
 export * from './checkbox-mapper';
 export * from './input-mapper';
+export * from './select-mapper';
 export * from './textarea-mapper';
