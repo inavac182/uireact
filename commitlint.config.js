8e0b8f2847d2169a939926220e6fbce137b4e4c7
Felipe Nava <felipe.nava.co@gmail.com>
2023-05-09 19:09:21 -0700

    feat(form): adding uiInput component (#85)

--- commitlint.config.js
+++ commitlint.config.js
@@ -20,6 +20,7 @@ module.exports = {
         'docs',
         'flexgrid',
         'foundation',
+        'form',
         'grid',
         'header',
         'icon',
---
+++ packages/form/src/ui-input.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import { TextSize, ThemeContext, getTextSize, getThemeStyling } from '@uireact/foundation';
+import { UiText } from '@uireact/text';
+
+import { UiInputProps, privateInputProps } from './types';
+import { InputMapper } from './theme';
+
+const Input = styled.input<privateInputProps>`
+  ${(props) => `
+    ${getThemeStyling(props.customTheme, props.selectedTheme, InputMapper)}
+    font-size: ${getTextSize(props.customTheme, TextSize.regular)};
+
+    border-style: solid;
+    border-width: 2px;
+    border-radius: 2px;
+
+    :focus {
+      border-style: solid;
+      border-width: 2px;
+      border-radius: 2px;
+    }
+
+    padding: 5px;
+    outline: none;
+  `}
+`;
+
+const WrapperDiv = styled.div`
+  display: flex;
+`;
+const LabelDiv = styled.div``;
+
+const InputDiv = styled.div`
+  display: inline-block;
+`;
+
+const Label = styled.label<privateInputProps>`
+  ${(props) => `
+    font-size: ${getTextSize(props.customTheme, TextSize.small)};
+  `}
+`;
+
+export const UiInput: React.FC<UiInputProps> = ({
+  error,
+  label,
+  labelOnTop,
+  name = 'input-name',
+  placeholder,
+  ref,
+  onChange,
+}: UiInputProps) => {
+  const theme = React.useContext(ThemeContext);
+
+  return (
+    <>
+      {label && labelOnTop && (
+        <LabelDiv>
+          <Label customTheme={theme.theme} selectedTheme={theme.selectedTheme} htmlFor={name}>
+            {label} &nbsp;
+          </Label>
+        </LabelDiv>
+      )}
+      <WrapperDiv>
+        {label && !labelOnTop && (
+          <LabelDiv>
+            <Label customTheme={theme.theme} selectedTheme={theme.selectedTheme} htmlFor={name}>
+              {label} &nbsp;
+            </Label>
+          </LabelDiv>
+        )}
+        <InputDiv>
+          <Input
+            ref={ref}
+            customTheme={theme.theme}
+            selectedTheme={theme.selectedTheme}
+            placeholder={placeholder}
+            onChange={onChange}
+            id={name}
+            name={name}
+          />
+          {error && <UiText>{error}</UiText>}
+        </InputDiv>
+      </WrapperDiv>
+    </>
+  );
+};
+
+UiInput.displayName = 'UiInput';
