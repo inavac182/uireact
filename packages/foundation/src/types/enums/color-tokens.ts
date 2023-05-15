f719d5160a266cb0610336eb767ca6d373d25299
Felipe Nava <felipe.nava.co@gmail.com>
2023-05-14 22:50:54 -0700

    docs(foundation): comment

--- packages/foundation/src/types/enums/color-tokens.ts
+++ packages/foundation/src/types/enums/color-tokens.ts
@@ -6,4 +6,11 @@ export enum ColorTokens {
   token_200 = 'token_200',
 }
 
+/**
+ * The intention of this type is that we can use it to set props from components that can have
+ * different weight colorations. E.G.
+ *
+ * <UiText weight="50" /> where `weight` correspond to a different level of token
+ */
+
 export type Token = '10' | '50' | '100' | '150' | '200';
