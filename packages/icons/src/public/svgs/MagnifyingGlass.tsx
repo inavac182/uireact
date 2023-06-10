b6d473973d3cf3c62b4e67757507162bbf1e5854
Felipe Nava <felipe.nava.co@gmail.com>
2023-06-09 21:10:20 -0700

    feat(icon): adding multimedia collection icons

---
+++ packages/icons/src/public/svgs/MagnifyingGlass.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgMagnifyingGlass = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" viewBox="0 0 451 451" width="1em" height="1em" {...props}>
+    <path d="m447.05 428-109.6-109.6c29.4-33.8 47.2-77.9 47.2-126.1C384.65 86.2 298.35 0 192.35 0 86.25 0 .05 86.3.05 192.3s86.3 192.3 192.3 192.3c48.2 0 92.3-17.8 126.1-47.2L428.05 447c2.6 2.6 6.1 4 9.5 4s6.9-1.3 9.5-4c5.2-5.2 5.2-13.8 0-19zM26.95 192.3c0-91.2 74.2-165.3 165.3-165.3 91.2 0 165.3 74.2 165.3 165.3s-74.1 165.4-165.3 165.4c-91.1 0-165.3-74.2-165.3-165.4z" />
+  </svg>
+);
+export default SvgMagnifyingGlass;
---
+++ packages/icons/src/public/svgs/Tag.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgTag = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    viewBox="0 0 487.85 487.85"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <path d="m479 215.275-83.5-113.9c-11-15-33.3-26.3-51.8-26.3H39.2c-21.6 0-39.2 17.6-39.2 39.2v259.3c0 21.6 17.6 39.2 39.2 39.2h304.5c18.6 0 40.9-11.3 51.8-26.3l83.5-113.9c11.8-16 11.8-41.2 0-57.3zm-21.8 41.5-83.5 113.9c-5.8 8-20.2 15.2-30.1 15.2H39.2c-6.7 0-12.2-5.5-12.2-12.2v-259.4c0-6.7 5.5-12.2 12.2-12.2h304.5c9.9 0 24.2 7.3 30.1 15.2l83.5 113.9c4.8 6.7 4.8 18.9-.1 25.6z" />
+  </svg>
+);
+export default SvgTag;
