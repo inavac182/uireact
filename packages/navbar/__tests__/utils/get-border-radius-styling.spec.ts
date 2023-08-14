d7e8f3d6dca77cfbb5943c7ca02f2d59b3ef9c7b
Felipe Nava <felipe.nava.co@gmail.com>
2023-08-14 10:45:21 -0700

    fix(navbar): bg color for active elements (#225)

--- packages/navbar/__tests__/utils/get-border-radius-styling.spec.ts
+++ packages/navbar/__tests__/utils/get-border-radius-styling.spec.ts
@@ -4,24 +4,24 @@ describe('getBorderRadiusStyling()', () => {
   it('Should get correct style for inline first', () => {
     const style = getBorderRadiusStyling('inline', true, false);
 
-    expect(style).toBe('border-radius: 5px 0 0 5px;');
+    expect(style).toBe('border-radius: 3px 0 0 3px;');
   });
 
   it('Should get correct style for inline last', () => {
     const style = getBorderRadiusStyling('inline', false, true);
 
-    expect(style).toBe('border-radius: 0 5px 5px 0;');
+    expect(style).toBe('border-radius: 0 3px 3px 0;');
   });
 
   it('Should get correct style for stacked first', () => {
     const style = getBorderRadiusStyling('stacked', true, false);
 
-    expect(style).toBe('border-radius: 5px 5px 0 0;');
+    expect(style).toBe('border-radius: 3px 3px 0 0;');
   });
 
   it('Should get correct style for stacked last', () => {
     const style = getBorderRadiusStyling('stacked', false, true);
 
-    expect(style).toBe('border-radius: 0 0 5px 5px;');
+    expect(style).toBe('border-radius: 0 0 3px 3px;');
   });
 });
