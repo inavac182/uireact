61de2aef341a1e66fa2fc255cc565b810afd1609
Felipe Nava <felipe.nava.co@gmail.com>
2023-08-25 00:40:22 -0700

    docs: update docs

--- packages/validator/docs/util/validator.tsx
+++ packages/validator/docs/util/validator.tsx
@@ -3,18 +3,15 @@ import React from 'react';
 import { UiText } from '@uireact/text';
 
 import { UiValidator } from '../../src';
+import { UiValidatorSchema } from 'types';
 
 type ValidatorRunnerProps = {
   data: { [keys in string]: unknown };
+  schema: UiValidatorSchema;
 };
 
-export const ValidatorRunner = ({ data }: ValidatorRunnerProps): React.ReactElement => {
+export const ValidatorRunner = ({ data, schema }: ValidatorRunnerProps): React.ReactElement => {
   const validator = new UiValidator();
-
-  const schema = {
-    firstName: validator.ruler().isRequired('First Name is required').range(0, 10, 'First name is not valid'),
-    email: validator.ruler().isRequired('Email is required').type('email', 'Email is not valid'),
-  };
 
   return (
     <UiText>
