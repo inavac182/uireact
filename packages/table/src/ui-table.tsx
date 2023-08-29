288f3e7b9f5660e86fbd554d302841a52eff1a88
Felipe Nava <felipe.nava.co@gmail.com>
2023-08-29 13:32:15 -0700

    feat(table): adding filter text box

--- packages/table/src/ui-table.tsx
+++ packages/table/src/ui-table.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FormEvent, useCallback, useState } from 'react';
 
 import styled from 'styled-components';
 
@@ -7,9 +7,13 @@ import {
   ColorTokens,
   ThemeContext,
   UiReactElementProps,
+  UiSpacing,
+  UiSpacingProps,
   getColorCategory,
   getThemeColor,
 } from '@uireact/foundation';
+import { UiInput } from '@uireact/form';
+import { UiIcon } from '@uireact/icons';
 
 import { UiTableData } from './types';
 import { privateTableProps } from './types/private-table-props';
@@ -17,6 +21,7 @@ import { privateTableProps } from './types/private-table-props';
 export type UiTableProps = {
   data: UiTableData;
   category?: ColorCategory;
+  withFilter?: boolean;
 } & UiReactElementProps;
 
 const Table = styled.table<privateTableProps>`
@@ -43,35 +48,72 @@ const TableCol = styled.td`
   text-align: center;
 `;
 
-export const UiTable: React.FC<UiTableProps> = ({ className, data, category = 'primary', testId }: UiTableProps) => {
+const iconSpacing: UiSpacingProps['margin'] = { top: 'two', left: 'four' };
+
+export const UiTable: React.FC<UiTableProps> = ({
+  className,
+  data,
+  category = 'primary',
+  testId,
+  withFilter = true,
+}: UiTableProps) => {
   const theme = React.useContext(ThemeContext);
+  const [_data, setPrivateData] = useState(data);
+  const [filterPhrase, setFilterPhrase] = useState('');
+
+  const onFilter = useCallback(
+    (e: FormEvent<HTMLInputElement>) => {
+      const newFilterPhrase = e.currentTarget.value;
+      setFilterPhrase(newFilterPhrase);
+
+      const filteredData = data.fields.filter(
+        (field) => field.filter((text) => text.includes(newFilterPhrase)).length > 0
+      );
+
+      setPrivateData({ ...data, fields: filteredData });
+    },
+    [setFilterPhrase]
+  );
 
   return (
-    <Table
-      className={className}
-      data-testid={testId}
-      $customTheme={theme.theme}
-      $selectedTheme={theme.selectedTheme}
-      $category={category}
-      cellSpacing="0"
-    >
-      <thead>
-        <tr>
-          {data.headings.map((heading, index) => (
-            <TableHeadingCol key={`table-heading-${index}`}>{heading}</TableHeadingCol>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.fields.map((field, rowIndex) => (
-          <tr key={`table-item-index-${rowIndex}`}>
-            {field.map((text, index) => (
-              <TableCol key={`table-item-${rowIndex}-colum-index-${index}`}>{text}</TableCol>
+    <div>
+      {withFilter && (
+        <UiInput
+          value={filterPhrase}
+          onChange={onFilter}
+          icon={
+            <UiSpacing margin={iconSpacing}>
+              <UiIcon icon="MagnifyingGlass" />
+            </UiSpacing>
+          }
+        />
+      )}
+      <Table
+        className={className}
+        data-testid={testId}
+        $customTheme={theme.theme}
+        $selectedTheme={theme.selectedTheme}
+        $category={category}
+        cellSpacing="0"
+      >
+        <thead>
+          <tr>
+            {_data.headings.map((heading, index) => (
+              <TableHeadingCol key={`table-heading-${index}`}>{heading}</TableHeadingCol>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {_data.fields.map((field, rowIndex) => (
+            <tr key={`table-item-index-${rowIndex}`}>
+              {field.map((text, index) => (
+                <TableCol key={`table-item-${rowIndex}-colum-index-${index}`}>{text}</TableCol>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
