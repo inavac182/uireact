8f6d53fb8d0c106be13049f51482dbfc7a8fb683
Felipe Nava <felipe.nava.co@gmail.com>
2023-09-18 00:23:05 -0700

    fix: examples versions

--- examples/vite-ui-react/src/App.tsx
+++ examples/vite-ui-react/src/App.tsx
@@ -12,12 +12,12 @@ function App() {
 
   return (
     <>
-      <UiView theme={DefaultTheme} $selectedTheme={ThemeColor.light}>
+      <UiView theme={DefaultTheme} selectedTheme={ThemeColor.light}>
         <div>
-          <a href="https://vitejs.dev" target="_blank">
+          <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
             <img src={viteLogo} className="logo" alt="Vite logo" />
           </a>
-          <a href="https://react.dev" target="_blank">
+          <a href="https://react.dev" target="_blank" rel="noreferrer">
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
