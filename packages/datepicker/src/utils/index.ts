0ea32f5a8bde66a57cb7af9b2fab6b6a14d61827
Felipe Nava <felipe.nava.co@gmail.com>
2023-11-09 15:45:47 -0800

    feat: adding datepicker input

--- packages/datepicker/src/utils/index.ts
+++ packages/datepicker/src/utils/index.ts
@@ -3,3 +3,5 @@ export * from './get-starting-day-of-the-week';
 export * from './get-day-title';
 export * from './get-days-by-week';
 export * from './get-month-title';
+export * from './get-padding';
+export * from './get-formatted-date';
