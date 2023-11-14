1609c41bb582cddf81b01769c756bfcecc54e061
Felipe Nava <felipe.nava.co@gmail.com>
2023-11-13 19:15:50 -0800

    feat: adding noitication options

--- packages/notifications/__tests__/ui-notifications.spec.tsx
+++ packages/notifications/__tests__/ui-notifications.spec.tsx
@@ -16,6 +16,7 @@ describe('<UiNotifications />', () => {
 
     expect(screen.getByRole('heading', { name: 'New notification' })).toBeVisible();
     expect(screen.getByText('This is a new notification')).toBeVisible();
+    expect(screen.getByTestId('notification-close-button')).toBeVisible();
 
     act(() => {
       jest.advanceTimersByTime(5000);
@@ -27,11 +28,34 @@ describe('<UiNotifications />', () => {
   it('renders fine with link', () => {
     uiRender(<NotificationsExample />);
 
-    fireEvent.click(screen.getByRole('button', { name: 'Add a notification with link' }));
+    fireEvent.click(screen.getByRole('button', { name: 'Notification with link' }));
 
     expect(screen.getByRole('heading', { name: 'Notification with link' })).toBeVisible();
     expect(screen.getByText('This is a new notification that shows a link')).toBeVisible();
     expect(screen.getByRole('link', { name: 'Ui React docs' })).toBeVisible();
     expect(screen.getByRole('link', { name: 'Ui React docs' })).toHaveAttribute('href', 'https://uireact.io');
+    expect(screen.getByTestId('notification-close-button')).toBeVisible();
+
+    fireEvent.click(screen.getByTestId('notification-close-button'));
+
+    expect(screen.queryByRole('heading', { name: 'Notification with link' })).not.toBeInTheDocument();
+  });
+
+  it('renders fine without close button', () => {
+    uiRender(<NotificationsExample />);
+
+    fireEvent.click(screen.getByRole('button', { name: 'Notification with options' }));
+
+    expect(screen.getByRole('heading', { name: 'Notification with options' })).toBeVisible();
+    expect(screen.queryByTestId('notification-close-button')).not.toBeInTheDocument();
+  });
+
+  it('renders fine with no timer', () => {
+    uiRender(<NotificationsExample />);
+
+    fireEvent.click(screen.getByRole('button', { name: 'No timer' }));
+
+    expect(screen.getByRole('heading', { name: 'Notification with no timer' })).toBeVisible();
+    expect(screen.queryByTestId('notification-close-button')).toBeVisible();
   });
 });
--- packages/notifications/src/ui-notifications.tsx
+++ packages/notifications/src/ui-notifications.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 
 import { NotificationsContainer, UiNotificationWrapper } from './private';
