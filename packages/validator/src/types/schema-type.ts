338b7823f6fdf838715fbbc7278af260a685f463
Felipe Nava <felipe.nava.co@gmail.com>
2023-11-26 15:08:39 -0800

    feat: adding validator use cases

--- packages/validator/src/types/schema-type.ts
+++ packages/validator/src/types/schema-type.ts
@@ -9,6 +9,14 @@ export type UiValidatorExpectationRule<T> = {
   error: UiValidatorError;
 };
 
+/** Rule that holds a value to compare with a baseline */
+export type UiValidatorComparisonRule = {
+  /** The expected value for this rule */
+  baseline: number | Date;
+  /** The error message to provide in the response if this check doesn't pass */
+  error: UiValidatorError;
+};
+
 /** The value for each rule */
 export type UiValidatorLengthRule = {
   /** The expected min length for this rule */
@@ -29,16 +37,32 @@ export type UiValidatorRangeRule = {
   error: UiValidatorError;
 };
 
+/** The value for each rule */
+export type UiValidatorDateRangeRule = {
+  /** The expected min range for this rule */
+  min: Date;
+  /** The expected max range for this rule */
+  max: Date;
+  /** The error message to provide in the response if this check doesn't pass */
+  error: UiValidatorError;
+};
+
 /** Set of possible rules for each field */
 export type UiValidatorRules = {
   /** Validates the type of the value */
-  type?: UiValidatorExpectationRule<'string' | 'numeric' | 'email' | 'phone'>;
+  type?: UiValidatorExpectationRule<'string' | 'numeric' | 'email' | 'phone' | 'date'>;
   /** Validates the nullability of the value */
   required?: UiValidatorExpectationRule<boolean>;
+  /** Validates the length of characters of the value */
+  dateRange?: UiValidatorDateRangeRule;
   /** Validates the length of characters of the value */
   range?: UiValidatorRangeRule;
   /** Validates the length of characters of the value */
   length?: UiValidatorLengthRule;
+  /** Validates a value is greater than a baseline */
+  greaterThan?: UiValidatorComparisonRule;
+  /** Validates a value is less than a baseline */
+  lessThan?: UiValidatorComparisonRule;
 };
 
 /** The schema needed for the UiValidator */
