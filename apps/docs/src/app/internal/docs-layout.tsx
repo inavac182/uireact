594b97cbec67bace8b7f1090b7b1dce8a52587aa
Felipe Nava <felipe.nava.co@gmail.com>
2024-03-19 23:24:32 -0700

    feat: header for docs

--- apps/docs/src/app/internal/docs-layout.tsx
+++ apps/docs/src/app/internal/docs-layout.tsx
@@ -2,10 +2,13 @@
 import React from 'react';
 
 import { UiFlexGrid, UiFlexGridItem } from '@uireact/flex';
-import { UiText } from '@uireact/text';
+import { UiLink, UiText } from '@uireact/text';
 import { UiSpacing, UiSpacingProps, UiViewport } from '@uireact/foundation';
+import { UiHeader } from '@uireact/header';
 
 import styled from 'styled-components';
+import { Logo } from './logo';
+import { UiIcon } from '@uireact/icons';
 
 type DocsLayoutProps = {
   children?: React.ReactNode;
@@ -24,26 +27,49 @@ const SideSection = styled.div`
   border-right: solid 3px var(--tertiary-token_100);
 `;
 
+const Container = styled.div`
+  position: absolute;
+  width: 100%;
+`;
 
+const HeaderSpacing: UiSpacingProps['padding'] = { inline: 'six' };
 const MainSectionSpacing: UiSpacingProps['padding'] = { all: 'five'};
 
 export const DocsLayout = ({ children }: DocsLayoutProps) => {
   return (
-    <UiFlexGrid columnGap='four'>
-      <UiViewport criteria={'l|xl'}>
-        <UiFlexGridItem>
-          <SideSection>
-            <UiText>Side</UiText>
-          </SideSection>
-        </UiFlexGridItem>
-      </UiViewport>
-      <UiFlexGridItem grow={1}>
-        <UiSpacing padding={MainSectionSpacing}>
-          <MainSection>
-            {children}
-          </MainSection>
+    <Container>
+      <UiHeader fixed>
+        <UiSpacing padding={HeaderSpacing}>
+          <UiFlexGrid alignItems='center' justifyContent='space-between'>
+            <UiFlexGridItem>
+              <Logo />
+            </UiFlexGridItem>
+            <UiFlexGridItem>
+              <UiLink>
+                <a href="https://uireact.io">
+                  <UiIcon icon='BrandsGithub' />
+                </a>
+              </UiLink>
+            </UiFlexGridItem>
+          </UiFlexGrid>
         </UiSpacing>
-      </UiFlexGridItem>
-    </UiFlexGrid>
+      </UiHeader>
+      <UiFlexGrid columnGap='four'>
+        <UiViewport criteria={'l|xl'}>
+          <UiFlexGridItem>
+            <SideSection>
+              <UiText>Side</UiText>
+            </SideSection>
+          </UiFlexGridItem>
+        </UiViewport>
+        <UiFlexGridItem grow={1}>
+          <UiSpacing padding={MainSectionSpacing}>
+            <MainSection>
+              {children}
+            </MainSection>
+          </UiSpacing>
+        </UiFlexGridItem>
+      </UiFlexGrid>
+    </Container>
   )
 };
