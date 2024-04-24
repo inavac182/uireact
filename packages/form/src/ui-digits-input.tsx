b7f46c87ff51bfc06baad7dfd186aee5beb7c1ec
Felipe Nava <felipe.nava.co@gmail.com>
2024-04-23 22:39:32 -0700

    fix: ts

--- packages/form/src/ui-digits-input.tsx
+++ packages/form/src/ui-digits-input.tsx
@@ -134,7 +134,7 @@ export const UiDigitsInput: React.FC<UiDigitsInputProps> = ({
               key={`internal-digit-${id}-input`}
               onChange={onDigitChange}
               required={required}
-              ref={(el) => inputsRef.current[id] = el}
+              ref={(el) => inputsRef.current[id] = el as HTMLInputElement}
               disabled={disabled}
             />
           ))}
