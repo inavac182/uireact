9ae0def3c2b56a8065ff646e9c6494645542066a
Felipe Nava <felipe.nava.co@gmail.com>
2024-04-24 20:10:00 -0700

    feat: sticky sidebar

--- apps/docs/src/app/docs/components/sidebar.tsx
+++ apps/docs/src/app/docs/components/sidebar.tsx
@@ -12,12 +12,16 @@ const SidebarWrapper = styled.div<{ $isFloatingNavbar?: boolean }>`
         `: `
             width: 300px;
             height: 100%;
-            position: sticky;
             padding: 30px;
             box-sizing: border-box;
             border-right: 3px solid var(--primary-token_150);
         `}
     `}
+`;
+
+const SidebarInnerContent = styled.div`
+    position: sticky;
+    top: 60px;
 `;
 
 type SideBarProps = {
@@ -37,15 +41,17 @@ export const SideBar = ({ isFloatingNavbar }: SideBarProps) => {
 
     return (
         <SidebarWrapper $isFloatingNavbar={isFloatingNavbar}>
-            <NavbarSection title='Components' icon='Layers' items={ComponentsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={1} selected={selected} />
-            <NavbarSection title='Charts' icon='ChartHistogram' items={ChartsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={2} selected={selected} />
-            <NavbarSection title='Dialogs' icon='CheckComment' items={DialogsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={3} selected={selected} />
-            <NavbarSection title='Forms' icon='EditSquare' items={FormsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={4} selected={selected} />
-            <NavbarSection title='Foundation' icon='LayoutFluid' items={FoundationSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={5} selected={selected} />
-            <NavbarSection title='Grids' icon='LayoutGrid' items={GridsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={6} selected={selected} />
-            <NavbarSection title='Typography' icon='Edit' items={TypographySideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={7} selected={selected} />
-            <NavbarSection title='Utils' icon='Settings' items={UtilsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={8} selected={selected} />
-            <NavbarSection title='Main View' icon='Computer' items={ViewSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={9} selected={selected} />
+            <SidebarInnerContent>
+                <NavbarSection title='Components' icon='Layers' items={ComponentsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={1} selected={selected} />
+                <NavbarSection title='Charts' icon='ChartHistogram' items={ChartsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={2} selected={selected} />
+                <NavbarSection title='Dialogs' icon='CheckComment' items={DialogsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={3} selected={selected} />
+                <NavbarSection title='Forms' icon='EditSquare' items={FormsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={4} selected={selected} />
+                <NavbarSection title='Foundation' icon='LayoutFluid' items={FoundationSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={5} selected={selected} />
+                <NavbarSection title='Grids' icon='LayoutGrid' items={GridsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={6} selected={selected} />
+                <NavbarSection title='Typography' icon='Edit' items={TypographySideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={7} selected={selected} />
+                <NavbarSection title='Utils' icon='Settings' items={UtilsSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={8} selected={selected} />
+                <NavbarSection title='Main View' icon='Computer' items={ViewSideBarData} isFloatingNavbar={isFloatingNavbar} onSelectMenu={onSelectMenu} id={9} selected={selected} />
+            </SidebarInnerContent>
         </SidebarWrapper>
     )
 };
