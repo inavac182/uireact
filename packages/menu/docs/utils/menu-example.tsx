313df2b7ae4a0b55f34cb7f650d45b0fbba46e5b
Felipe Nava <felipe.nava.co@gmail.com>
2024-04-29 21:43:14 -0700

    BREAKING CHANGE: :adding framer motion to UiMenu

--- packages/menu/docs/utils/menu-example.tsx
+++ packages/menu/docs/utils/menu-example.tsx
@@ -1,11 +1,20 @@
 'use client';
 import React from 'react';
 
-import { UiButton } from '@uireact/button';
-import { UiText } from '@uireact/text';
+import { MotionProps } from 'framer-motion';
 
+import { UiButton } from '@uireact/button';
+import { UiCard } from '@uireact/card';
+import { UiText } from '@uireact/text';
+import { UiReactFadeLeft, UiReactFadeUp } from '@uireact/framer-animations';
 import { UiMenu } from '@uireact/menu';
-import { UiSpacing } from '@uireact/foundation';
+import { UiSpacing, UiSpacingProps } from '@uireact/foundation';
+import { UiIcon } from '@uireact/icons';
+import { UiFlexGrid } from '@uireact/flex';
+
+const buttonSpacing: UiSpacingProps['padding'] = {
+  block: 'four'
+}
 
 export const MenuExample: React.FC = () => {
   const [isVisible, setIsVisible] = React.useState(false);
@@ -23,7 +32,7 @@ export const MenuExample: React.FC = () => {
       <div>
         <UiButton onClick={handleMenuOpen}>Open Menu</UiButton>
       </div>
-      <UiMenu visible={isVisible} closeMenuCB={closeMenu} fullscreenOnSmall>
+      <UiMenu visible={isVisible} closeMenuCB={closeMenu} fullscreenOnSmall motion={UiReactFadeUp}>
         <UiSpacing margin={{ all: 'five' }}>
           <UiText centered>Menu Content</UiText>
           <div>
@@ -42,6 +51,65 @@ export const MenuExample: React.FC = () => {
           varius lacus varius aliquet lobortis. Curabitur vitae urna diam.
         </UiText>
       </UiSpacing>
+    </UiSpacing>
+  );
+};
+
+const getDelayedAnimation = (index: number): MotionProps => {
+  return { ...UiReactFadeLeft, transition: { delay: 0.2 * index } };
+};
+
+export const AnimatedMenuExample: React.FC = () => {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const handleMenuOpen = React.useCallback(() => {
+    setIsVisible(true);
+  }, [setIsVisible]);
+
+  const closeMenu = React.useCallback(() => {
+    setIsVisible(false);
+  }, [setIsVisible]);
+
+  return (
+    <UiSpacing padding={{ all: 'five' }}>
+      <div>
+        <UiButton onClick={handleMenuOpen}>Open Menu</UiButton>
+      </div>
+      <UiMenu visible={isVisible} closeMenuCB={closeMenu} fullscreenOnSmall motion={UiReactFadeUp}>
+        <UiSpacing margin={{ all: 'five' }}>
+          <UiFlexGrid gap='four' direction='column'>
+            <UiCard motion={getDelayedAnimation(1)} styling='outlined' category='tertiary'>
+              <UiFlexGrid gap='four'>
+                <UiIcon icon="BatteryFull" category='tertiary' />
+                <p>Option 1</p>
+              </UiFlexGrid>
+            </UiCard>
+            <UiCard motion={getDelayedAnimation(2)} styling='outlined' category='secondary'>
+              <UiFlexGrid gap='four'>
+                <UiIcon icon="Heart" category='secondary' />
+                <p>Option 2</p>
+              </UiFlexGrid>
+            </UiCard>
+            <UiCard motion={getDelayedAnimation(3)} styling='outlined' category='warning'>
+              <UiFlexGrid gap='four'>
+                <UiIcon icon="Coffee" category='warning' />
+                <p>Option 3</p>
+              </UiFlexGrid>
+            </UiCard>
+            <UiCard motion={getDelayedAnimation(4)} styling='outlined' category='positive'>
+              <UiFlexGrid gap='four'>
+                <UiIcon icon="City" category='positive' />
+                <p>Option 4</p>
+              </UiFlexGrid>
+            </UiCard>
+            <UiButton onClick={closeMenu} category="secondary" fullWidth>
+              <UiSpacing padding={buttonSpacing}>
+                Close menu
+              </UiSpacing>
+            </UiButton>
+          </UiFlexGrid>
+        </UiSpacing>
+      </UiMenu>
     </UiSpacing>
   );
 };
