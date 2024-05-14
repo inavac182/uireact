e74f0982893ac356ec3868a02fe2b99279de095b
Felipe Nava <felipe.nava.co@gmail.com>
2024-05-13 18:07:16 -0700

    feat(animations): adding icon animations props

--- packages/framer-animations/src/on-hover.ts
+++ packages/framer-animations/src/on-hover.ts
@@ -11,3 +11,24 @@ export const UiReactHoverElevate: MotionProps = {
         y: -10
     }
 };
+
+export const UiReactHoverDance: MotionProps = {
+    whileHover: {
+        rotate: [0, 45, -45, 0],
+        transition: {
+            duration: .5,
+            repeat: Infinity,
+            repeatDelay: 1
+        }
+    }
+};
+
+export const UiReactHoverShake: MotionProps = {
+    whileHover: {
+        rotate: [0, 45, -45, 0],
+        transition: {
+            duration: .5,
+            repeat: Infinity
+        }
+    }
+};
