53b83ebba40ae8296d03d2de9a6ca8839421c7dc
Felipe Nava <felipe.nava.co@gmail.com>
2024-05-14 22:56:54 -0700

    fix(docs): generate docs page for bundled package

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,6 +1,15 @@
-const { readdirSync, existsSync, mkdirSync, rmSync, cpSync } =  require('fs');
+const { readdirSync, existsSync, mkdirSync, rmSync, cpSync, renameSync, writeFileSync } =  require('fs');
 
 const baseDocsAppPath = `./apps/docs/src/app/docs/(pages)`;
+
+const docPageContent = `
+"use client";
+import Doc from "./doc.mdx";
+
+export default function Page() {
+  return <Doc />;
+}
+`;
 
 console.log('Compiling doc pages...');
 
@@ -41,13 +50,20 @@ directories.map((directory) => {
   if (singleDocPage) {
     // Copy files from package folder to docs app
     packageDocsEntries.forEach((entry) => {
-      cpSync(`${entry.path}/${entry.name}`, `${packageDocFolderRoute}${entry.name}`, { recursive: true, force: true });
+      if (entry.name === "page.mdx") {
+        cpSync(`${entry.path}/${entry.name}`, `${packageDocFolderRoute}doc.mdx`, { recursive: true, force: true });  
+        writeFileSync(`${packageDocFolderRoute}page.tsx`, docPageContent);
+      } else {
+        cpSync(`${entry.path}/${entry.name}`, `${packageDocFolderRoute}${entry.name}`, { recursive: true, force: true });
+      }
     });
   } else {
     packageDocsEntries.forEach((entry) => {
       const packageDocFolderRoute = `${baseDocsAppPath}/${entry.name}/`;
 
       cpSync(`${entry.path}/${entry.name}`, packageDocFolderRoute, { recursive: true, force: true });
+      renameSync(`${packageDocFolderRoute}page.mdx`, `${packageDocFolderRoute}doc.mdx`);
+      writeFileSync(`${packageDocFolderRoute}page.tsx`, docPageContent);
     });
   }
   
