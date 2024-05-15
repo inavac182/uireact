e9e784860c93ff55fe456b00835a7fce3cd6061f
Felipe Nava <felipe.nava.co@gmail.com>
2024-05-15 01:57:37 -0700

    test: fix test

--- packages/notifications/__tests__/ui-notifications.spec.tsx
+++ packages/notifications/__tests__/ui-notifications.spec.tsx
@@ -1,10 +1,108 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { act, fireEvent, screen } from '@testing-library/react';
 
 import { uiRender } from '../../../__tests__/utils/render';
 
-import { NotificationsExample } from '../docs/example';
+import { UiButton } from '@uireact/button';
+import { UiFlexGrid } from '@uireact/flex';
+import { UiSpacingProps, UiSpacing } from '@uireact/foundation';
+import { UiText } from '@uireact/text';
+import { useNotifications, UiNotifications } from '../src';
+
+const buttonContentPadding: UiSpacingProps['padding'] = { block: 'four' };
+
+type NotificationsExampleProps = {
+  onNotificationShown?: () => void;
+  onNotificationDismissed?: () => void;
+  onLinkClicked?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+};
+
+export const NotificationsExample: React.FC<NotificationsExampleProps> = ({
+  onNotificationShown,
+  onNotificationDismissed,
+  onLinkClicked,
+}: NotificationsExampleProps) => {
+  const { showNotification } = useNotifications();
+
+  const addNotification = useCallback(() => {
+    showNotification({
+      icon: 'Check',
+      title: 'New notification',
+      message: 'This is a new notification',
+    });
+  }, [showNotification]);
+
+  const addLinkNotification = useCallback(() => {
+    showNotification({
+      icon: 'Link',
+      title: 'Notification with link',
+      message: 'This is a new notification that shows a link',
+      link: {
+        label: 'Ui React docs',
+        url: 'https://uireact.io',
+      },
+      options: {
+        onNotificationShown,
+        onNotificationDismissed,
+        onLinkClicked
+      },
+    });
+  }, [showNotification, onNotificationShown, onNotificationDismissed, onLinkClicked]);
+
+  const addNotificationWithOptions = useCallback(() => {
+    showNotification({
+      icon: 'ArrowTrendDown',
+      title: 'Notification with options',
+      options: {
+        category: 'error',
+        closeButton: false,
+        timer: 25000,
+      },
+    });
+  }, [showNotification]);
+
+  const addNotificationWithNoTimer = useCallback(() => {
+    showNotification({
+      icon: 'ArrowTrendDown',
+      title: 'Notification with no timer',
+      message: 'This notification has to be closed otherwise will always be present',
+      options: {
+        category: 'warning',
+        timer: 0,
+      },
+    });
+  }, [showNotification]);
+
+  return (
+    <>
+      <UiNotifications />
+      <UiFlexGrid columnGap="four">
+        <UiButton onClick={addNotification} category="positive">
+          <UiSpacing padding={buttonContentPadding}>
+            <UiText>Add notification</UiText>
+          </UiSpacing>
+        </UiButton>
+        <UiButton onClick={addLinkNotification} category="tertiary">
+          <UiSpacing padding={buttonContentPadding}>
+            <UiText>Notification with link</UiText>
+          </UiSpacing>
+        </UiButton>
+        <UiButton onClick={addNotificationWithOptions} category="error">
+          <UiSpacing padding={buttonContentPadding}>
+            <UiText>Notification with options</UiText>
+          </UiSpacing>
+        </UiButton>
+        <UiButton onClick={addNotificationWithNoTimer} category="warning">
+          <UiSpacing padding={buttonContentPadding}>
+            <UiText>No timer</UiText>
+          </UiSpacing>
+        </UiButton>
+      </UiFlexGrid>
+    </>
+  );
+};
+
 
 describe('<UiNotifications />', () => {
   it('renders fine', () => {
