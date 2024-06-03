2ef56945a1daba90a3490c2cc561d85b62428de2
Felipe Nava <felipe.nava.co@gmail.com>
2024-06-03 15:51:47 -0700

    feat: migrating menu to scss

--- packages/menu/src/types/menu-props.ts
+++ packages/menu/src/types/menu-props.ts
@@ -15,15 +15,3 @@ export type UiMenuProps = {
   /** CB for when menu is closed */
   closeMenuCB: () => void;
 } & UiReactElementProps;
-
-export type privateMenuProps = {
-  /** Close label for dialog on small devices */
-  $closeLabel?: string;
-  /** If the menu should go to fullscreen on small breakpoint */
-  $fullscreenOnSmall?: boolean;
-  /** Menu identifier to be used when is fullscreen on small */
-  $menuId?: string;
-  /** Menu visibility, default FALSE */
-  $visible: boolean;
-  $isOffset: boolean;
-};
