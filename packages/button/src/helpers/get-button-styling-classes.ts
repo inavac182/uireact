985c614ec54fd598c421159433250b52ad6353f2
Felipe Nava <felipe.nava.co@gmail.com>
2024-06-05 10:25:37 -0700

    refactor(button): lighter colors for outlined button

--- packages/button/src/helpers/get-button-styling-classes.ts
+++ packages/button/src/helpers/get-button-styling-classes.ts
@@ -7,11 +7,11 @@ export const getButtonStylingClasses = (styling: 'filled' | 'clear' | 'icon' | '
     }
 
     if (styling === 'clear') {
-        return `${styles.buttonClear} hover-bg-${category}-100 active-bg-${category}-200`;
+        return `${styles.buttonClear} hover-bg-${category}-10 active-bg-${category}-50`;
     }
 
     if (styling === 'outlined') {
-        return `${styles.buttonOutlined} border-${category}-100 hover-bg-${category}-100 active-bg-${category}-200 color-${category}-100 hover-color-${category}-10`;
+        return `${styles.buttonOutlined} border-${category}-100 hover-bg-${category}-10 active-bg-${category}-50 color-${category}-100 hover-color-${category}-100`;
     }
 
     return `bg-${category}-100 border-${category}-150 hover-bg-${category}-150 active-bg-${category}-200`;
