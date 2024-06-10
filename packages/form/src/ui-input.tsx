4ce63299743b188332c53459ae2fd891c06a30c1
Felipe Nava <felipe.nava.co@gmail.com>
2024-06-09 20:26:11 -0700

    feat(input): adding padding prop (#355)

--- packages/form/src/ui-input.tsx
+++ packages/form/src/ui-input.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import { UiText, UiLabel } from '@uireact/text';
+import { SpacingDistribution, getSpacingClass } from '@uireact/foundation';
 
 import { UiInputProps } from './types';
 import styles from './ui-input.scss';
+
+const defaultPadding: SpacingDistribution = { block: 'three' };
 
 export const UiInput: React.FC<UiInputProps> = ({
   className = '',
@@ -20,47 +23,51 @@ export const UiInput: React.FC<UiInputProps> = ({
   type,
   value,
   onChange,
-  size,
+  size = 'regular',
+  padding = defaultPadding,
   required,
   ...props
-}: UiInputProps) => (
-  <div data-testid={testId}>
-    {label && labelOnTop && (
-      <div>
-        <UiLabel htmlFor={name} category={category === 'primary' ? 'fonts' : category}>
-          {label} &nbsp;
-        </UiLabel>
-      </div>
-    )}
-    <div className='wrapperInput'>
-      {label && !labelOnTop && (
+}: UiInputProps) => {
+  const paddingClass = getSpacingClass('padding', padding);
+
+  return (
+    <div data-testid={testId}>
+      {label && labelOnTop && (
         <div>
           <UiLabel htmlFor={name} category={category === 'primary' ? 'fonts' : category}>
             {label} &nbsp;
           </UiLabel>
         </div>
       )}
-      <div className={styles.inputDiv}>
-        <div className={styles.inputContentDiv}>
-          {icon && <div className={styles.inputIconContainer}>{icon}</div>}
-          <input
-            disabled={disabled}
-            className={`${className} ${styles.input} bg-primary-10 border-${category ? category : 'primary'}-100 focus-border-${category ? category : 'tertiary'}-100 ${icon ? styles.inputIcon : ''}`}
-            id={name}
-            name={name}
-            onChange={onChange}
-            placeholder={placeholder}
-            ref={ref}
-            type={type}
-            value={value}
-            required={required}
-            {...props}
-          />
+      <div className='wrapperInput'>
+        {label && !labelOnTop && (
+          <div>
+            <UiLabel htmlFor={name} category={category === 'primary' ? 'fonts' : category}>
+              {label} &nbsp;
+            </UiLabel>
+          </div>
+        )}
+        <div className={styles.inputDiv}>
+          <div className={styles.inputContentDiv}>
+            {icon && <div className={styles.inputIconContainer}>{icon}</div>}
+            <input
+              disabled={disabled}
+              className={`${className} ${styles.input} ${paddingClass} bg-primary-10 border-${category ? category : 'primary'}-100 focus-border-${category ? category : 'tertiary'}-100 ${icon ? styles.inputIcon : ''} size-${size}`}
+              id={name}
+              name={name}
+              onChange={onChange}
+              placeholder={placeholder}
+              ref={ref}
+              type={type}
+              value={value}
+              required={required}
+              {...props}
+            />
+          </div>
+          {error && <UiText category={category}>{error}</UiText>}
         </div>
-        {error && <UiText category={category}>{error}</UiText>}
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 UiInput.displayName = 'UiInput';
