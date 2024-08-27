1727f52ccb753a6c60039b20f7f33f026b764ea6
Felipe Nava <felipe.nava.co@gmail.com>
2024-08-26 20:35:05 -0700

    test: update unit tests

--- packages/button/__tests__/tertiary.spec.tsx
+++ packages/button/__tests__/tertiary.spec.tsx
@@ -32,7 +32,7 @@ describe('<UiTertiaryButton />', () => {
     uiRender(<UiTertiaryButton>MyButton</UiTertiaryButton>);
 
     expect(screen.getByRole('button', { name: 'MyButton' })).toBeVisible();
-    expect(screen.getByRole('button')).toHaveClass('button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 padding-top-three padding-bottom-three padding-left-four padding-right-four buttonRadius');
+    expect(screen.getByRole('button')).toHaveClass('size-regular button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 padding-top-one padding-bottom-one padding-left-two padding-right-two buttonRadius');
   });
 
   test('renders data test id', () => {
@@ -49,7 +49,7 @@ describe('<UiTertiaryButton />', () => {
     );
 
     expect(screen.getByTestId('my-button')).toBeDisabled();
-    expect(screen.getByRole('button')).toHaveClass('button transparent bg-primary-100 color-secondary-50 padding-top-three padding-bottom-three padding-left-four padding-right-four buttonRadius');
+    expect(screen.getByRole('button')).toHaveClass('size-regular button transparent bg-primary-100 color-secondary-50 padding-top-one padding-bottom-one padding-left-two padding-right-two buttonRadius');
   });
 
   test('renders disabled icon', () => {
@@ -60,7 +60,7 @@ describe('<UiTertiaryButton />', () => {
     );
 
     expect(screen.getByTestId('my-button')).toBeDisabled();
-    expect(screen.getByRole('button')).toHaveClass('button transparent bg-primary-100 color-secondary-50 padding-top-three padding-bottom-three padding-left-four padding-right-four buttonIcon');
+    expect(screen.getByRole('button')).toHaveClass('size-regular button transparent bg-primary-100 color-secondary-50 padding-top-one padding-bottom-one padding-left-two padding-right-two buttonIcon');
   }); 
 
   test('renders rounded', () => {
@@ -70,21 +70,21 @@ describe('<UiTertiaryButton />', () => {
       </UiTertiaryButton>
     );
 
-    expect(screen.getByRole('button')).toHaveClass('button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 padding-top-three padding-bottom-three padding-left-four padding-right-four radius-regular');
+    expect(screen.getByRole('button')).toHaveClass('size-regular button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 padding-top-one padding-bottom-one padding-left-two padding-right-two radius-regular');
   }); 
 
   test('renders full width', () => {
     uiRender(<UiTertiaryButton fullWidth>MyButton</UiTertiaryButton>);
 
     expect(screen.getByRole('button')).toBeVisible();
-    expect(screen.getByRole('button')).toHaveClass('button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 buttonFullWidth padding-top-three padding-bottom-three padding-left-four padding-right-four buttonRadius');
+    expect(screen.getByRole('button')).toHaveClass('size-regular button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 buttonFullWidth padding-top-one padding-bottom-one padding-left-two padding-right-two buttonRadius');
   });
 
   test('renders full height', () => {
     uiRender(<UiTertiaryButton fullHeight>MyButton</UiTertiaryButton>);
 
     expect(screen.getByRole('button')).toBeVisible();
-    expect(screen.getByRole('button')).toHaveClass('button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 buttonFullHeight padding-top-three padding-bottom-three padding-left-four padding-right-four buttonRadius');
+    expect(screen.getByRole('button')).toHaveClass('size-regular button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 buttonFullHeight padding-top-one padding-bottom-one padding-left-two padding-right-two buttonRadius');
   });
 
   test('renders class name', () => {
@@ -122,6 +122,6 @@ describe('<UiTertiaryButton />', () => {
     );
 
     expect(screen.getByRole('button', { name: 'Button' })).toBeVisible();
-    expect(screen.getByRole('button')).toHaveClass('button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 padding-top-three padding-bottom-three padding-left-four padding-right-four margin-two buttonRadius');
+    expect(screen.getByRole('button')).toHaveClass('size-regular button transparent hover-bg-primary-100 color-secondary-100 active-bg-primary-150 padding-top-one padding-bottom-one padding-left-two padding-right-two margin-two buttonRadius');
   });
 });
