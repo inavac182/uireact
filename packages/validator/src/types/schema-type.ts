2cf4dfc9503d2c1c94f3f4685111c99b06a5b1ac
Felipe Nava <felipe.nava.co@gmail.com>
2024-09-21 11:59:24 -0700

    feat(validator): adding one of check

--- packages/validator/src/types/schema-type.ts
+++ packages/validator/src/types/schema-type.ts
@@ -18,6 +18,14 @@ export type UiValidatorComparisonRule = {
   error: UiValidatorError;
 };
 
+/** Rule that holds a value to compare with a baseline */
+export type UiValidatorChoicesRule = {
+  /** The expected value for this rule */
+  options: Array<string | number>;
+  /** The error message to provide in the response if this check doesn't pass */
+  error: UiValidatorError;
+};
+
 /** The value for each rule */
 export type UiValidatorLengthRule = {
   /** The expected min length for this rule */
@@ -49,25 +57,7 @@ export type UiValidatorDateRangeRule = {
 };
 
 /** The possible field types for each rule */
-export type UiValidatorFieldTypes = 'text' | 'numeric' | 'email' | 'phone' | 'date' | 'boolean';
-
-/** Set of possible rules for each field */
-export type UiValidatorRules = {
-  /** Validates the type of the value */
-  type?: UiValidatorExpectationRule<UiValidatorFieldTypes>;
-  /** Validates the nullability of the value */
-  required?: UiValidatorExpectationRule<boolean>;
-  /** Validates the length of characters of the value */
-  dateRange?: UiValidatorDateRangeRule;
-  /** Validates the length of characters of the value */
-  range?: UiValidatorRangeRule;
-  /** Validates the length of characters of the value */
-  length?: UiValidatorLengthRule;
-  /** Validates a value is greater than a baseline */
-  greaterThan?: UiValidatorComparisonRule;
-  /** Validates a value is less than a baseline */
-  lessThan?: UiValidatorComparisonRule;
-};
+export type UiValidatorFieldTypes = 'text' | 'numeric' | 'email' | 'phone' | 'date' | 'boolean' | 'choice';
 
 /** Set of possible rules for each field */
 export type UiValidatorFieldRules = {
@@ -85,6 +75,8 @@ export type UiValidatorFieldRules = {
   greaterThan?: UiValidatorComparisonRule;
   /** Validates a value is less than a baseline */
   lessThan?: UiValidatorComparisonRule;
+  /** Validates that a choice field is one of a given set of values */
+  oneOf?: UiValidatorChoicesRule
 };
 
 /** Metadata information used from EzForms */
