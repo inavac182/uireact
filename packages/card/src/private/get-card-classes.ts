f5af0a5bd45526b305f0510042469a16039baac1
Felipe Nava <felipe.nava.co@gmail.com>
2025-11-17 17:43:17 -0800

    fix: color inside outlined card

--- packages/card/src/private/get-card-classes.ts
+++ packages/card/src/private/get-card-classes.ts
@@ -4,7 +4,7 @@ import styles from '../ui-card.scss';
 
 export const getCardClasses = (styling: 'filled' | 'outlined', category: ColorCategory, weight: ColorToken): string => {
     if (styling === 'outlined') {
-        return `${styles.cardOutlined} border-${category}-100 color-${category}-100`;
+        return `${styles.cardOutlined} border-${category}-100`;
     }
 
     return `bg-${category}-${weight} color-fonts-100`;
